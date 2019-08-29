cee9009c56d8e517f82262c325ca93477a5170cf
munshidun <munshidun@gmail.com>
2019-08-29 19:27:03 +0500

    Create branch schema/controller

--- app/resolvers.js
+++ app/resolvers.js
@@ -3,6 +3,7 @@ const data = require( './data.js' );
 module.exports = {
 	Query: {
 		staff: () => data.users,
+		branches: () => data.branches,
 		employee: (_,{ id }) => data.users.find( user => user.id === id )
 	}
 }
--- app/schema.js
+++ app/schema.js
@@ -1,7 +1,14 @@
 module.exports = `
 	type Query {
 		staff: [Employee]
+		branches: [Branch]
 		employee(id: Int): Employee
+	}
+
+	type Branch {
+		id: Int
+		name: String
+		code: String
 	}
 
 	type Employee {
