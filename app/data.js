a6294698d29cbdb5938a0a7575052c94fb21b057
munshidun <munshidun@gmail.com>
2019-08-29 18:19:43 +0500

    Create user data

---
+++ app/data.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+	users: [
+		{
+			id: 1,
+			firstname: "John",
+			lastname: "Smith",
+			middlename: null
+		},
+		{
+			id: 2,
+			firstname: "Catarina",
+			lastname: "Vivian",
+			middlename: null
+		}
+	]
+};
